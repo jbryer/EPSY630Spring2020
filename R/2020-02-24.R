645d91fe4850c96cef419bcd558aa862d181446e
Jason Bryer <jason@bryer.org>
2020-02-24 18:22:08 -0500

    Added R script.

---
+++ R/2020-02-24.R
@@ -0,0 +1,17 @@
+ggplot(reportCard, aes(x = Pass2012, y = Pass2013)) + 
+	geom_point() + xlim(c(0,100)) + ylim(c(0,100))
+
+ggplot(reportCard, aes(x = Pass2012, y = Pass2013)) + 
+	geom_point() + xlim(c(0,100)) + ylim(c(0,100)) +
+	geom_smooth(method = 'lm')
+
+ggplot(reportCard, aes(x = Pass2012, y = Pass2013)) + 
+	geom_point() + xlim(c(0,100)) + ylim(c(0,100)) +
+	geom_smooth(method = 'lm') +
+	geom_smooth(formula = Pass2013 ~ Pass2012^2 + Pass2012, color = 'red')
+
+ggplot(reportCard, aes(x = Pass2012, y = Pass2013)) + 
+	geom_point(alpha = 0.1) + xlim(c(0,100)) + ylim(c(0,100)) +
+	geom_smooth(method = 'lm') +
+	geom_smooth(formula = Pass2013 ~ Pass2012^2 + Pass2012, color = 'red') +
+	geom_smooth(method = 'loess', color = 'green', span = 1)
