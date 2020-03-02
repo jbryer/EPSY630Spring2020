37cf597e8769f316e10d8e9ab446075ac4d9a6db
Jason Bryer <jason@bryer.org>
2020-03-02 16:25:03 -0500

    Added slides.

--- R/2020-02-24.R
+++ R/2020-02-24.R
@@ -1,3 +1,5 @@
+load('course_data/NYSReportCard-Grade7Math.Rda')
+
 ggplot(reportCard, aes(x = Pass2012, y = Pass2013)) + 
 	geom_point() + xlim(c(0,100)) + ylim(c(0,100))
 
@@ -15,3 +17,8 @@ ggplot(reportCard, aes(x = Pass2012, y = Pass2013)) +
 	geom_smooth(method = 'lm') +
 	geom_smooth(formula = Pass2013 ~ Pass2012^2 + Pass2012, color = 'red') +
 	geom_smooth(method = 'loess', color = 'green', span = 1)
+
+lm.out <- lm(Pass2013 ~ Pass2012, data = reportCard)
+summary(lm.out)
+
+plot(reportCard$Pass2012, resid(lm.out))
